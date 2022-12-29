a6c62ca00228ea69904bb828003ba67bb9ce28fa
kirby1926 <bakj@seas.upenn.edu>
2022-12-29 15:35:20 -0500

    fixed text movement, starting to add navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import './App.css';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function App() {
   return (
     <div className="App">
       <div class="wrapper">
+        
+        <nav>
+          <div class="nav-menu-section">
+            <a href="#">
+              <FontAwesomeIcon icon="fa-solid fa-bars-sort" />
+            </a>
+          </div>
+          <div class="nav-logo-section">
+            <a href="#">
+              <FontAwesomeIcon icon="fa-sharp fa-solid fa-j" />
+
+              {/* THEN TRANSITION TO THIS WHEN YOU CLICK IT: <FontAwesomeIcon icon="fa-solid fa-bars-filter" /> */}
+
+            </a>
+          </div>
+          <div class="nav-social-section">
+            <a href="#">
+              <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+            <a href='#'>
+              <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
+            </a>
+            <a href="#">
+              <FontAwesomeIcon icon="fa-brands fa-soundcloud" />
+            </a>
+          </div>
+        </nav>
+
+        <div class="titles">
+          <h1 class="title">James Baker</h1>
+          <h2 class="subtitle">cooking up amazing things</h2>
+        </div>
+        
         <header>
           <img src={require('./pics/background-removed/belevedere-PhotoRoom.png-PhotoRoom.png')} alt="Foreground" class="foreground"/>
           <img src={require('./pics/background-removed/background_rm.png')} alt="Background" class="background"/>
-          <div class="box">
-            <h1 class="title">JAMES BAKER</h1>
-          </div>
         </header>
 
         <br />
