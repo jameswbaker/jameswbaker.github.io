96f43c62f280e19cbf99bcdc9dea3d8cde57b68d
kirby1926 <bakj@seas.upenn.edu>
2024-06-01 14:45:44 -0400

    vids

--- src/Teaching.js
+++ src/Teaching.js
@@ -6,8 +6,8 @@ function Teaching() {
         <div>
             <h3>Teaching</h3>
             <div className="projects_array">
-                <TeachingCard addr="https://drive.google.com/drive/folders/1LaA7wGzVV_2clCcrQ6oF3c_AvNIq09AR?usp=drive_link" title="Math 2400: Recitation Notes" subtitle="Spring 2024"/>
-                <TeachingCard addr="https://drive.google.com/drive/folders/1-jms686NRwDO7xLsmlKSAPcO2tFeNIGB?usp=sharing" title="Math 2400: Recitation Notes" subtitle="Fall 2024"/>
+                <TeachingCard addr="https://drive.google.com/drive/folders/1LaA7wGzVV_2clCcrQ6oF3c_AvNIq09AR?usp=drive_link" title="Math 2400: Spring 2024" subtitle="Recitation Notes"/>
+                <TeachingCard addr="https://drive.google.com/drive/folders/1-jms686NRwDO7xLsmlKSAPcO2tFeNIGB?usp=sharing" title="Math 2400: Fall 2023" subtitle="Recitation Notes"/>
             </div>
             
             
